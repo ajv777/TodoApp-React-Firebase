d449a9fe89295242f1a59cb90e37927f3f9636b5
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-07-20 12:00:57 +0200

    Create a promise to return all the task from Firebase

--- src/components/AddTaks/AddTaks.js
+++ src/components/AddTaks/AddTaks.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+// Library Lodash to validate the form, isEmpty return true or false
+import { isEmpty } from "lodash";
+import firebase from "../../utils/firebase";
+import "firebase/firestore";
 import { ReactComponent as Send } from "../../assets/send.svg";
 
 import "./AddTaks.scss";
 
+// Save task in database
+const dbTodo = firebase.firestore(firebase);
+
 export default function AddTaks() {
   const [task, setTask] = useState("");
+
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log("Form has send");
-    console.log("Task has send", task);
+    if (!isEmpty(task)) {
+      dbTodo
+        .collection("tasks")
+        .add({
+          name: task,
+          completed: false,
+        })
+        .then(() => {
+          setTask("");
+          console.log("the task has saved in database");
+        });
+    }
   };
+  // console.log("Form has send");
+  // console.log("Task has send", task);
+  // console.log("task is empty?", isEmpty(task));
+
   return (
     <Form onSubmit={onSubmit} className="add-task">
       <input

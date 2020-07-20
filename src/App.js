d412e1bbfa231e44282e3c6f200cacc2caee9529
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-07-20 14:49:22 +0200

    add new components

--- src/App.js
+++ src/App.js
@@ -1,76 +1,73 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { map } from "lodash";
+import { map, size } from "lodash";
 import firebase from "./utils/firebase";
 import "firebase/firestore";
 
-import AddTaks from "./components/AddTaks";
-
+import AddTask from "./components/AddTaks";
+import Task from "./components/Task";
 import "./App.scss";
 
 const dbTodo = firebase.firestore(firebase);
 
 export default function App() {
-  // useState
-  const [tasks, setTask] = useState([]);
-  console.log(tasks);
+  const [tasks, setTasks] = useState([]);
+  const [reloadTasks, setReloadTasks] = useState(false);
 
   useEffect(() => {
     dbTodo
       .collection("tasks")
-      // The taks completed (true) appear at the end of the list
       .orderBy("completed")
       .get()
       .then((response) => {
-        // Inicialized an array empty to save each task with 3 properties: name, completed and id
-        const arrayTaks = [];
-        //console.log(response);
-        // Function to iterate an array. For each iteration, return one task
-        // .data is a function to return the data of each task
-        map(response.docs, (task) => {
-          // console.log(task.data());
-          // console.log(task.id);
+        const arrayTasks = [];
+        map(response?.docs, (task) => {
           const data = task.data();
           data.id = task.id;
-          // console.log(data);
-          arrayTaks.push(data);
+          arrayTasks.push(data);
         });
-        //console.log(arrayTaks);
-        setTask(arrayTaks);
-      })
-      .catch((err) => {
-        console.log("error", err);
+        setTasks(arrayTasks);
       });
-  }, []);
+    setReloadTasks(false);
+  }, [reloadTasks]);
 
   return (
     <Container fluid className="app">
       <div className="title">
         <h1>Ana Junquera Vara</h1>
       </div>
+
       <Row className="todo">
         <Col
           className="todo-title"
           xs={{ span: 10, offset: 1 }}
           md={{ span: 6, offset: 3 }}
         >
-          <h2>Today</h2>
-        </Col>
-        <Col
-          className="todo-list"
-          xs={{ span: 10, offset: 1 }}
-          md={{ span: 6, offset: 3 }}
-        >
-          <p>Todo List</p>
+          <h2>Tasks for today</h2>
         </Col>
         <Col
           className="todo-input"
           xs={{ span: 10, offset: 1 }}
           md={{ span: 6, offset: 3 }}
         >
-          <AddTaks></AddTaks>
+          <AddTask setReloadTasks={setReloadTasks} />
+        </Col>
+        <Col
+          className="todo-list"
+          xs={{ span: 10, offset: 1 }}
+          md={{ span: 6, offset: 3 }}
+        >
+          {size(tasks) === 0 ? (
+            <h4>You are free! There aren´t tasks for today</h4>
+          ) : (
+            map(tasks, (task) => (
+              <Task task={task} setReloadTasks={setReloadTasks} />
+            ))
+          )}
         </Col>
       </Row>
     </Container>
   );
 }
+
+// Map: iterate the array of tasks and shown as taks as we have. Get task by props to Task component.
